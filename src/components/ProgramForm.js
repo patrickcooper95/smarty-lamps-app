79277d084d4d3158beb19585f541b266bc1e480d
Patrick <patrickcooper1995@gmail.com>
2020-11-09 00:04:44 -0500

    110920 - Initial UI testing110920

---
+++ src/components/ProgramForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+        />
+      </form>
+    );
+  }
+}
+export default MyForm
+//ReactDOM.render(<MyForm />, document.getElementById('root'));

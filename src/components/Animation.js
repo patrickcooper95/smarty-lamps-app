2893ec2b60b428fdb0c48b37c22712589a7bf266
patrickcooper95 <patrickcooper1995@gmail.com>
2021-04-13 00:00:40 -0400

    041221 - first working live color animation

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSpring, animated } from 'react-spring'
 import range from 'lodash-es/range'
 import './styles.css'
@@ -7,7 +7,21 @@ import './styles.css'
 const items = range(5)
 const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0)`
 
+function currentProgram() {
+  fetch("/devices/desk-led")
+    .then(response => response.json())
+    .then(data => {
+      var elements = document.getElementsByClassName("script-bf-box");
+      for (const x of Array(5).keys()) {
+        console.log(elements[x]);
+        elements[x].style.backgroundColor = data.data[0][3];
+      }
+    });
+}
+
+
 function Animation() {
+  currentProgram();
   const { radians } = useSpring({
     to: async next => {
       while (1) await next({ radians: 2 * Math.PI })
@@ -17,7 +31,7 @@ function Animation() {
     reset: true,
   })
   return items.map(i => <animated.div key={i} className="script-bf-box"
-  style={{ "transform": radians.interpolate(interp(i)), "background-color": "white" }} />)
+  style={{ "transform": radians.interpolate(interp(i)) }} />)
 }
 
 export default Animation
